ee3a36ccaa27a7ddc4395928fac9382567c74ff0
Daniel Bultas <danielbultas@gmail.com>
2021-08-24 12:14:30 +0200

    fix google maps scripts

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -27,26 +27,9 @@ $(document).ready(function(){
 
      $("#page1").show();
      $("#link1 a").addClass("active");
-
-
-    // Google maps
-      function initialize() {
-        var myLatlng = new google.maps.LatLng(49.713954,13.405821);
-        var mapOptions = {
-          zoom: 15,
-          center: myLatlng,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            title: 'K Řece 716/14'
-        });
-      }
-
-      google.maps.event.addDomListener(window, 'load', initialize);
+    
+    
+    
 
       // Flickr
       $('#basicuse').jflickrfeed({
